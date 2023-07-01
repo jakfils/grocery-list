f5a590eca699826953e841cdf4832f5431527c8a
jakfils <jakfilstechno@gmail.com>
2023-07-01 20:32:11 +0100

    Add submit button outline on focus

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
+const input = document.querySelector('input[type ="text"]');
+const submit = document.querySelector('input[type ="submit"]');
 
+input.addEventListener("focus", (e) => {
+  submit.style.outline = "solid 2px var(--button-background-color)";
+});
+input.addEventListener("focusout", (e) => {
+  submit.style.outline = "none";
+});
