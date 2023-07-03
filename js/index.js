884704f93debf0bb474ec9f6caeaf6539eb893cc
jakfils <jakfilstechno@gmail.com>
2023-07-03 16:51:04 +0100

    Fix Edit and Delete buttons event listener

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ const updateStorage = () => {
   localStorage.setItem("storageData", JSON.stringify(data));
 };
 
-// Function check input value is empty
+// Function check if input value is empty
 const inputIsEmpty = (str) => {
   if (!str.replace(/\s/g, "").length) {
     return true;
@@ -35,24 +35,37 @@ const showAlertMsg = (msgContent, backgroundColor) => {
   }, 2000);
 };
 
+// Funtion check if we display clear all items button and set paddig to items div
+const buttonClearAndItemsPaddingState = () => {
+  items.childNodes.length < 2
+    ? clearItems.classList.add("invisible")
+    : clearItems.classList.remove("invisible");
+  items.childNodes.length === 0
+    ? (items.style.padding = "0px")
+    : (items.style.padding = "2px");
+};
+
 // Function show items in the DOM
 const showItems = () => {
   const jsonData = localStorage.getItem("storageData");
   const objData = JSON.parse(jsonData);
-  //Check Data array is not null
+  //Check Data array is not null and map elements width HTML tags
   if (objData !== null) {
     const mappedData = objData.map((element) => {
       return `<div class="item">
-      <p class="item-name">${element}</p><span class="edit"><i class="fas fa-solid fa-pen"></i></span><span
-      class="delete"><i class="fas fa-regular fa-trash"></i></span>
+      <p class="item-name">${element}</p><div class="edit"><i class="fas fa-solid fa-pen"></i></div><div
+      class="delete"><i class="fas fa-regular fa-trash"></i></div>
       </div>`;
     });
+    // Join mapped elements
     const itemsContent = mappedData.join("");
+    // Push elements to the DOM
     items.innerHTML = itemsContent;
   }
 };
 
 showItems();
+buttonClearAndItemsPaddingState();
 
 const editBtns = document.querySelectorAll(".edit");
 let elementToEdit = "";
@@ -61,15 +74,17 @@ const deleteBtns = document.querySelectorAll(".delete");
 // On submit
 submit.addEventListener("click", (e) => {
   e.preventDefault();
-
+  // To add new item
   if (!inputIsEmpty(input.value) && submit.value === "Submit") {
     data.unshift(input.value);
     updateStorage();
     input.value = "";
     showItems();
-    document.location.reload();
     showAlertMsg("Item added to the list", "#6de676");
-  } else if (!inputIsEmpty(input.value) && submit.value === "Edit") {
+    buttonClearAndItemsPaddingState();
+  }
+  // To edit an item
+  else if (!inputIsEmpty(input.value) && submit.value === "Edit") {
     submit.value = "Submit";
     var nodes = Array.prototype.slice.call(
       document.querySelector(".items").children
@@ -78,57 +93,59 @@ submit.addEventListener("click", (e) => {
     data.splice(itemToEdit, 1, input.value);
     updateStorage();
     showItems();
-
     input.value = "";
     input.focus();
     showAlertMsg("Value changed", "#6de676");
-    document.location.reload();
-  } else {
-    document.location.reload();
+    buttonClearAndItemsPaddingState();
+  }
+
+  // Avoid input empty value
+  else {
     showAlertMsg("Please enter a value", "#e66b6c");
   }
 });
 
-editBtns.forEach((editBtn) => {
-  editBtn.addEventListener("click", (e) => {
+// Edit and Delete button click listener
+items.addEventListener("click", (e) => {
+  // Edit
+  if (e.target.classList.contains("fa-pen")) {
     submit.value = "Edit";
-    input.value = editBtn.previousSibling.textContent;
+    input.value = e.target.parentNode.previousSibling.textContent;
     input.focus();
-    elementToEdit = editBtn.previousSibling;
-  });
-});
+    elementToEdit = e.target.parentNode.previousSibling;
 
-deleteBtns.forEach((deleteBtn) => {
-  deleteBtn.addEventListener("click", (e) => {
+    // Delete
+  } else if (e.target.classList.contains("fa-trash")) {
     var nodes = Array.prototype.slice.call(
       document.querySelector(".items").children
     );
-    const itemToDeleteId = nodes.indexOf(deleteBtn.parentNode);
+    const itemToDeleteId = nodes.indexOf(e.target.parentNode.parentNode);
     data.splice(itemToDeleteId, 1);
     updateStorage();
     showItems();
     showAlertMsg("Item removed", "#e66b6c");
     input.focus();
-    // showItems();
-    document.location.reload();
-  });
+    input.value = "";
+    buttonClearAndItemsPaddingState();
+  }
 });
 
+// Clear all items button click listener
 clearItems.addEventListener("click", (e) => {
   data = [];
   updateStorage();
   showItems();
   input.focus();
-  document.location.reload();
+  input.value = "";
+  buttonClearAndItemsPaddingState();
 });
 
+// Input focus listener
 input.addEventListener("focus", (e) => {
   submit.style.outline = "solid 2px var(--button-background-color)";
 });
+
+// Input focus out listener
 input.addEventListener("focusout", (e) => {
   submit.style.outline = "none";
 });
-
-if (items.childNodes.length < 2) {
-  clearItems.classList.add("invisible");
-}
